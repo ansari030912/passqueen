ed78a3213c1f1ecf604de8ab34986630b14bddda
Hussnain <howrex95@gmail.com>
2024-09-18 12:55:47 +0500

    cd

--- app/vendor-exam-training/[vendor_perma]/[cert_perma]/page.jsx
+++ app/vendor-exam-training/[vendor_perma]/[cert_perma]/page.jsx
@@ -128,7 +128,7 @@ export async function generateMetadata({ params }) {
       other: [
         {
           rel: "canonical",
-          url: `https://passqueen.com/study-meterial/${params.vendor_perma}/${params.cert_perma}`,
+          url: `https://passqueen.com/vendor-exam-training/${params.vendor_perma}/${params.cert_perma}`,
         },
       ],
     },
