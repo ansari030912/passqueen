709973bee7cce54f9dd94b46c9f244f24239dfd5
Hussnain <howrex95@gmail.com>
2024-09-19 16:06:53 +0500

    sx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,9 @@ import { AuthGuard } from "./auth/AuthGuard";
 export const metadata = {
   title: `Updated Exam Training by Tech Professionals`,
   description: `PassQueen is a premium provider of Real and Valid Exam Training of IT certification Exams. Pass your certification exam easily with pdf and test engine exams in 2024.`,
+  verification: {
+    google: "rw75WZwh1RHDs_ByvgvO904yBLGJ5_DvmW5TkHoIUEs",
+  },
 };
 
 export default function RootLayout({ children }) {
